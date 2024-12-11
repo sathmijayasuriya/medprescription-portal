be7e18ff91178a28693b455ab49bf2e341e2b0ad
sathmijayasuriya <sathmijayasuriya@gmail.com>
2024-12-11 20:30:28 +0530

    initial commit

---
+++ backend/src/main/java/com/backend/MediEase/MediEaseApplication.java
@@ -0,0 +1,13 @@
+package com.backend.MediEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediEaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/MediEase/MediEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.MediEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
